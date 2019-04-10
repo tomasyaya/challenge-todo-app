d31504bdb79ff9f2484aa63a9e5ef0b6b4461f00
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-04-10 13:13:55 +0200

    added validation to the form

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -6,13 +6,16 @@ class AddTodo extends Component {
 
   state = {
     title: '',
-    body: ''
+    body: '',
+    validation: false,
+    message: 'Please complete all fields'
   }
 
   handleChange = (event) => {
     const { name, value } = event.target
     this.setState({
-      [name]: value
+      [name]: value,
+      validation: false
     })
   }
 
@@ -20,6 +23,12 @@ class AddTodo extends Component {
     event.preventDefault();
     const { body, title } = this.state;
     const { getAllTodos } = this.props;
+    if(!body.length || !title.length) {
+      this.setState({
+        validation: true
+      })
+      return
+    }
     const newTodo = {
       title,
       body,
@@ -38,12 +47,13 @@ class AddTodo extends Component {
   }
 
   render() {
-    const { title, body } = this.state;
+    const { title, body, validation, message } = this.state;
     console.log(this.props)
     return (
       <div className="add-todo-container">
         <h1>Todo List</h1>
         <form onSubmit={this.handleSubmit}>
+          {validation ? <p id="validation">{message}</p> : null}
           <input type="text" name="title" placeholder="title" value={title} onChange={this.handleChange}/>
           <input type="text" name="body" placeholder="message" value={body} onChange={this.handleChange}/>
           <button type="submit">Add Todo</button>
